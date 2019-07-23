c3293b9502d0ff8358acce378e04e55b07e2bddf
Roman Shapiro <eleroman@mail.ru>
2019-07-23 13:11:00 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# StbStbVorbisSharp
+[![NuGet](https://img.shields.io/nuget/v/StbStbVorbisSharp.svg)](https://www.nuget.org/packages/StbStbVorbisSharp/) [![Build status](https://ci.appveyor.com/api/projects/status/isyfkbfakrhoa1bm?svg=true)](https://ci.appveyor.com/project/RomanShapiro/stbStbVorbissharp)
+
+C# port of stb_vorbis.c
+
+## Credits
+* [stb](https://github.com/nothings/stb)
---
+++ samples/StbVorbisSharp.MonoGame.Test/Game1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace StbVorbisSharp.MonoGame.Test
+{
+	/// <summary>
+	/// This is the main type for your game.
+	/// </summary>
+	public class Game1 : Game
+	{
+		GraphicsDeviceManager _graphics;
+		SpriteBatch _spriteBatch;
+
+		private Vorbis _vorbis;
+		private DynamicSoundEffectInstance _effect;
+		private bool _startedPlaying;
+
+		public Game1()
+		{
+			_graphics = new GraphicsDeviceManager(this)
+			{
+				PreferredBackBufferWidth = 1400,
+				PreferredBackBufferHeight = 960
+			};
+
+			Content.RootDirectory = "Content";
+			IsMouseVisible = true;
+			Window.AllowUserResizing = true;
+		}
+		
+		private void SubmitBuffer()
+		{
+			_vorbis.SubmitBuffer();
+
+			if (_vorbis.Decoded == 0)
+			{
+				// Restart
+				_vorbis.Restart();
+				_vorbis.SubmitBuffer();
+			}
+
+			var audioShort = _vorbis.SongBuffer;
+			byte[] audioData = new byte[_vorbis.Decoded * _vorbis.Channels * 2];
+			for (var i = 0; i < _vorbis.Decoded * _vorbis.Channels; ++i)
+			{
+				if (i * 2 >= audioData.Length)
+				{
+					break;
+				}
+
+				var b1 = (byte)(audioShort[i] >> 8);
+				var b2 = (byte)(audioShort[i] & 256);
+
+				audioData[i * 2 + 0] = b2;
+				audioData[i * 2 + 1] = b1;
+			}
+
+			_effect.SubmitBuffer(audioData);
+		}
+
+		private void LoadSong()
+		{
+			var path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+			path = Path.Combine(path, "music.ogg");
+			var buffer = File.ReadAllBytes(path);
+
+			_vorbis = Vorbis.FromMemory(buffer);
+
+			_effect = new DynamicSoundEffectInstance(_vorbis.SampleRate, (AudioChannels)_vorbis.Channels)
+			{
+				Volume = 0.5f
+			};
+
+			_effect.BufferNeeded += (s, a) => SubmitBuffer();
+
+			SubmitBuffer();
+		}
+
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+			_spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			// TODO: use this.Content to load your game content here
+			LoadSong();
+
+			GC.Collect();
+		}
+
+		/// <summary>
+		/// UnloadContent will be called once per game and is the place to unload
+		/// game-specific content.
+		/// </summary>
+		protected override void UnloadContent()
+		{
+			// TODO: Unload any non ContentManager content here
+		}
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update(GameTime gameTime)
+		{
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+			    Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+
+			// TODO: Add your update logic here
+			if (!_startedPlaying)
+			{
+				_effect.Play();
+				_startedPlaying = true;
+			}
+
+			base.Update(gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			// TODO: Add your drawing code here
+			base.Draw(gameTime);
+		}
+	}
+}
